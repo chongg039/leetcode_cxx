37ce1b6eb1447fa33474603826e462e1b4a13700
GC <chongg039@gmail.com>
2020-01-15 20:07:09 +0800

    change solution 4

--- 4_median_two_sorted_arrays/Solution4.cc
+++ 4_median_two_sorted_arrays/Solution4.cc
@@ -4,90 +4,32 @@
 
 #include "Solution4.h"
 
-void Solution4::binaryInsert(vector<int> &vec, vector<int>::const_iterator middle, int ele) {
-    if (vec.empty()) {
-        return;
+// https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/4-xun-zhao-liang-ge-you-xu-shu-zu-de-zhong-wei-shu/
+double Solution4::findMedianSortedArrays(std::vector<int> &nums1, std::vector<int> &nums2)
+{
+    int n = nums1.size(), m = nums2.size();
+    if (n > m) {
+        // 用来保证nums1一定最短的方法
+        return findMedianSortedArrays(nums2, nums1);
     }
-    if (1 == vec.size()) {
-        if (ele >= *vec.cbegin()) {
-            vec.insert(vec.cend(), ele);
-        } else {
-            vec.insert(vec.cbegin(), ele);
-        }
-        return ;
-    }
-    if (ele >= *middle) {
-        // 若middle不在末尾
-        if (vec.cend() != middle+1) {
-            // 若ele小于等于middle+1，在middle+1插入
-            if (ele <= *(middle+1)) {
-                vec.insert(middle+1, ele);
-                return ;
-            } else {
-                af = {middle+1, vec.cend()};
-                middle += af.size() / 2 + 1;
-                af.clear();
-                binaryInsert(vec, middle, ele);
-            }
-        } else {
-            vec.insert(vec.cend(), ele);
-            return ;
-        }
-    } else {
-        // 若middle不在开头
-        if (vec.cbegin() != middle) {
-            // 若ele大于等于middle-1，在middle插入
-            if (ele >= *(middle-1)) {
-                vec.insert(middle, ele);
-                return ;
-            } else {
-                bf = {vec.cbegin(), middle};
-                middle -=  bf.size() / 2 + 1;
-                bf.clear();
-                binaryInsert(vec, middle, ele);
-            }
-        } else {
-            vec.insert(vec.cbegin(), ele);
-            return ;
-        }
-    }
-}
+    // 为了保证奇偶情况的相同，虚拟插入#，nums1长度变为了2n
+    int l_max1 = 0, l_max2 = 0, r_min1 = 0, r_min2 = 0, lo = 0, hi = 2 * n;
+    while (lo <= hi) {
+        int c1 = (lo + hi) / 2;
+        int c2 = m + n - c1;
 
-double Solution4::findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
-    if (nums1.empty() && nums2.empty()){
-        median = 0;
-    } else if (nums1.empty() && !nums2.empty()) {
-        if (0 == nums2.size() % 2) {
-            median = float(nums2.at(nums2.size() / 2 - 1) + nums2.at(nums2.size() / 2)) / 2;
-        } else {
-            median = nums2.at(nums2.size() / 2);
-        }
-    } else if (!nums1.empty() && nums2.empty()) {
-        if (0 == nums1.size() % 2) {
-            median = float(nums1.at(nums1.size() / 2 - 1) + nums1.at(nums1.size() / 2)) / 2;
-        } else {
-            median = nums1.at(nums1.size() / 2);
-        }
-    } else {
-        // nums1 和 nums2 都不为空，使用二分插入组合
-        for (auto it = nums2.cbegin(); it != nums2.cend(); ++it) {
-            size_t half_size = nums1.size() / 2;
-            auto middle_pos = nums1.cbegin() + half_size;
-            binaryInsert(nums1, middle_pos, *it);
-        }
-        if (0 == nums1.size() % 2) {
-            median = float(nums1.at(nums1.size() / 2 - 1) + nums1.at(nums1.size() / 2)) / 2;
-        } else {
-            median = nums1.at(nums1.size() / 2);
-        }
-    }
-    return median;
-}
+        l_max1 = (0 == c1) ? INT_MIN : nums1[(c1 - 1) / 2];
+        r_min1 = (2 * n == c1) ? INT_MAX : nums1[c1 / 2];
+        l_max2 = (0 == c2) ? INT_MIN : nums2[(c2 - 1) / 2];
+        r_min2 = (2 * m == c2) ? INT_MAX : nums2[c2 / 2];
 
-void Solution4::display(vector<int> &vec) {
-    for (auto it = vec.cbegin(); it != vec.cend(); ++it) {
-        cout << *it << " ";
+        if (l_max1 > r_min2) {
+            hi = c1 - 1;
+        } else if (l_max2 > r_min1) {
+            lo = c1 + 1;
+        } else {
+            break;
+        }
     }
-    cout << endl;
-    return ;
+    return (std::max(l_max1, l_max2) + std::min(r_min1, r_min2)) / 2.0;
 }
--- 4_median_two_sorted_arrays/Solution4.h
+++ 4_median_two_sorted_arrays/Solution4.h
@@ -6,23 +6,13 @@
 #define LEETCODE_SOLUTION4_H
 
 #include <vector>
+#include <climits>
 #include <iostream>
-
-using std::vector;
-using std::size_t;
-using std::cout;
-using std::endl;
-
+#include <algorithm>
 
 class Solution4 {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2);
-private:
-    void display(vector<int> &vec);
-    void binaryInsert(vector<int> &vec, vector<int>::const_iterator middle, int ele);
-    double median;
-    vector<int> bf;
-    vector<int> af;
+    static double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2);
 };
 
 
